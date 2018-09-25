5acc800efb230bb2d74195cc8e39ddc315336110
ULDBerry <39335342+ULDBerry@users.noreply.github.com>
2018-09-25 01:10:27 +0100

    Start of Total Points updating
    When the fixes buttons are pressed the total points now updates

--- ScoreCaclculator/app/src/main/java/ie/ul/dave/scorecaclculator/MainActivity.java
+++ ScoreCaclculator/app/src/main/java/ie/ul/dave/scorecaclculator/MainActivity.java
@@ -14,36 +14,51 @@ public class MainActivity extends AppCompatActivity {
     private int mWBPoints = 0;
     private int mPointsTotal = 0;
     private TextView mMessageColourPointsView;
+    private TextView mMessageTotalPointView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+     //Setup initial values to display
         mMessageColourPointsView = findViewById(R.id.message_colorptsfin);
         mMessageColourPointsView.setText(getString(R.string.message_colourpts,mColourPoints));
+
+        mMessageTotalPointView = findViewById(R.id.message_totalpoints);
+        mMessageTotalPointView.setText(getString(R.string.message_pointstotal, mPointsTotal));
+
 
     }
 
     public void button_fixes3(View view){
         mColourPoints = 0;
         mMessageColourPointsView.setText(getString(R.string.message_colourpts,mColourPoints));
+        updateTotal();
     }
 
     public void button_fixes2(View view){
         mColourPoints = 25;
         mMessageColourPointsView.setText(getString(R.string.message_colourpts,mColourPoints));
+        updateTotal();
     }
 
     public void button_fixes1(View view){
         mColourPoints = 75;
         mMessageColourPointsView.setText(getString(R.string.message_colourpts,mColourPoints));
+        updateTotal();
     }
 
     public void button_fixes0(View view){
         mColourPoints = 150;
         mMessageColourPointsView.setText(getString(R.string.message_colourpts,mColourPoints));
+        updateTotal();
     }
 
+    private void updateTotal(){
+        mPointsTotal = mColourPoints;
+        mMessageTotalPointView.setText(getString(R.string.message_pointstotal, mPointsTotal));
+
+    }
 
 }
